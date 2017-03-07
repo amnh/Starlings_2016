21b2054497a35138328368b484f2fc5251c42727
tina <tinaw425@gmail.com>
2017-03-07 16:51:28 -0500

    primary keys

--- starlings_observer/starling_base/models.py
+++ starlings_observer/starling_base/models.py
@@ -14,7 +14,7 @@ class BasicTraits(models.Model):
     fat = models.TextField(db_column='FAT', blank=True, null=True)  # Field name made lowercase.
     age = models.TextField(db_column='AGE', blank=True, null=True)  # Field name made lowercase.
     sex = models.TextField(db_column='SEX', blank=True, null=True)  # Field name made lowercase.
-    basic_traits_id = models.TextField(db_column='Basic_Traits_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    basic_traits_id = models.TextField(db_column='Basic_Traits_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -22,7 +22,7 @@ class BasicTraits(models.Model):
 
 
 class BirdInfo(models.Model):
-    number = models.TextField(db_column='NUMBER', blank=True, null=True)  # Field name made lowercase.
+    number = models.TextField(db_column='NUMBER', blank=True, null=True, primary_key=True)  # Field name made lowercase.
     complex_traits_id = models.ForeignKey(Complex_Traits)  # Field name made lowercase. This field type is a guess.
     location_id = models.ForeignKey(Location)  # Field name made lowercase. This field type is a guess.
     preparation_id = models.ForeignKey(Preparation)  # This field type is a guess.
@@ -38,7 +38,7 @@ class Collection(models.Model):
     date_collected = models.TextField(db_column='DATE_COLLECTED', blank=True, null=True)  # Field name made lowercase.
     arrived_amnh = models.TextField(db_column='Arrived_AMNH', blank=True, null=True)  # Field name made lowercase.
     collector_s_field = models.TextField(db_column='COLLECTOR(S)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
-    collection_id = models.TextField(db_column='Collection_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    collection_id = models.TextField(db_column='Collection_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -51,7 +51,7 @@ class ComplexTraits(models.Model):
     testes_l = models.TextField(db_column='TESTES_L', blank=True, null=True)  # Field name made lowercase.
     ovaries = models.TextField(db_column='OVARIES', blank=True, null=True)  # Field name made lowercase.
     basic_traits_id = models.ForeignKey(Basic_Traits)  # Field name made lowercase. This field type is a guess.
-    complex_traits_id = models.TextField(db_column='Complex_Traits_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    complex_traits_id = models.TextField(db_column='Complex_Traits_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -62,7 +62,7 @@ class Death(models.Model):
     prep = models.TextField(db_column='PREP', blank=True, null=True)  # Field name made lowercase.
     trap = models.TextField(db_column='TRAP', blank=True, null=True)  # Field name made lowercase.
     depredation_method = models.TextField(db_column='DEPREDATION_METHOD', blank=True, null=True)  # Field name made lowercase.
-    death_id = models.TextField(db_column='Death_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    death_id = models.TextField(db_column='Death_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -72,7 +72,7 @@ class Death(models.Model):
 class Identifiers(models.Model):
     identification = models.TextField(db_column='IDENTIFICATION', blank=True, null=True)  # Field name made lowercase.
     country = models.TextField(db_column='COUNTRY', blank=True, null=True)  # Field name made lowercase.
-    identifiers_id = models.TextField(db_column='Identifiers_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    identifiers_id = models.TextField(db_column='Identifiers_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -87,7 +87,7 @@ class Location(models.Model):
     longitude = models.TextField(db_column='LONGITUDE', blank=True, null=True)  # Field name made lowercase.
     collection_id = models.ForeignKey(Collection)  # Field name made lowercase. This field type is a guess.
     identifiers_id = models.ForeignKey(Identifiers)  # Field name made lowercase. This field type is a guess.
-    location_id = models.TextField(db_column='Location_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    location_id = models.TextField(db_column='Location_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -102,7 +102,7 @@ class PreSkin(models.Model):
     tail_length_mm_pre_skin_field = models.TextField(db_column='Tail_length_mm_(Pre-Skin)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
     tarsus_length_mm_pre_skin_field = models.TextField(db_column='Tarsus_length_mm_(Pre-Skin)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
     notes_pre_skin_field = models.TextField(db_column='Notes_(Pre-Skin)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
-    pre_skin_id = models.TextField(db_column='Pre_Skin_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    pre_skin_id = models.TextField(db_column='Pre_Skin_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -113,7 +113,7 @@ class Preparation(models.Model):
     specimen_prep = models.TextField(db_column='SPECIMEN_PREP', blank=True, null=True)  # Field name made lowercase.
     preparator = models.TextField(db_column='PREPARATOR', blank=True, null=True)  # Field name made lowercase.
     death_id = models.ForeignKey(Death)  # Field name made lowercase. This field type is a guess.
-    preparation_id = models.TextField(db_column='Preparation_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    preparation_id = models.TextField(db_column='Preparation_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
@@ -128,7 +128,7 @@ class Skin(models.Model):
     tail_length_mm_skin_field = models.TextField(db_column='Tail_length_mm_(Skin)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
     tarsus_length_mm_skin_field = models.TextField(db_column='Tarsus_length_mm_(Skin)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
     notes_skin_field = models.TextField(db_column='Notes_(Skin)', blank=True, null=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'.
-    skin_id = models.TextField(db_column='Skin_ID', blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    skin_id = models.TextField(db_column='Skin_ID', blank=True, null=True, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
