e0a52c867817aac66ebd8be7ad1349de5f59f528
Ashley <afrazier316@gmail.com>
2017-03-09 16:41:12 -0500

    Foreign Key Edit Part Three

--- starlings_observer/starling_base/models.py
+++ starlings_observer/starling_base/models.py
@@ -39,7 +39,7 @@ class ComplexTraits(models.Model):
     testes_r = models.TextField(db_column='TESTES_R', blank=True, null=True)  # Field name made lowercase.
     testes_l = models.TextField(db_column='TESTES_L', blank=True, null=True)  # Field name made lowercase.
     ovaries = models.TextField(db_column='OVARIES', blank=True, null=True)  # Field name made lowercase.
-    basic_traits_id = models.ForeignKey('BasicTraits', on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    basic_traits = models.ForeignKey('BasicTraits', on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
     complex_traits_id = models.TextField(db_column='Complex_Traits_ID', blank=True, null=False, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
@@ -74,8 +74,8 @@ class Location(models.Model):
     precise_locality = models.TextField(db_column='PRECISE_LOCALITY', blank=True, null=True)  # Field name made lowercase.
     latitude = models.TextField(db_column='LATITUDE', blank=True, null=True)  # Field name made lowercase.
     longitude = models.TextField(db_column='LONGITUDE', blank=True, null=True)  # Field name made lowercase.
-    collection_id = models.ForeignKey(Collection, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
-    identifiers_id = models.ForeignKey(Identifiers, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    identifiers = models.ForeignKey(Identifiers, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
     location_id = models.TextField(db_column='Location_ID', blank=True, null=False, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
@@ -101,7 +101,7 @@ class PreSkin(models.Model):
 class Preparation(models.Model):
     specimen_prep = models.TextField(db_column='SPECIMEN_PREP', blank=True, null=True)  # Field name made lowercase.
     preparator = models.TextField(db_column='PREPARATOR', blank=True, null=True)  # Field name made lowercase.
-    death_id = models.ForeignKey(Death, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    death = models.ForeignKey(Death, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
     preparation_id = models.TextField(db_column='Preparation_ID', blank=True, null=False, primary_key=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
@@ -126,11 +126,11 @@ class Skin(models.Model):
 
 class BirdInfo(models.Model):
     number = models.TextField(db_column='NUMBER', blank=True, null=False, primary_key=True)  # Field name made lowercase.
-    complex_traits_id = models.ForeignKey('ComplexTraits', on_delete=models.CASCADE, blank=True, null=True,)  # Field name made lowercase. This field type is a guess.
-    location_id = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
-    preparation_id = models.ForeignKey(Preparation, on_delete=models.CASCADE, blank=True, null=True)  # This field type is a guess.
-    pre_skin_id = models.ForeignKey('PreSkin', on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
-    skin_id = models.ForeignKey(Skin, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    complex_traits = models.ForeignKey('ComplexTraits', on_delete=models.CASCADE, blank=True, null=True,)  # Field name made lowercase. This field type is a guess.
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    preparation = models.ForeignKey(Preparation, on_delete=models.CASCADE, blank=True, null=True)  # This field type is a guess.
+    pre_skin = models.ForeignKey('PreSkin', on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
+    skin = models.ForeignKey(Skin, on_delete=models.CASCADE, blank=True, null=True)  # Field name made lowercase. This field type is a guess.
 
     class Meta:
         managed = False
